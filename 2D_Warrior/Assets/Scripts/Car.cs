4f0ea6523edb38f214f5f2d4dc81512522a44750
KID <50666207+KID421@users.noreply.github.com>
2020-12-20 14:31:29 +0800

    欄位、四大類型與修飾詞

--- 2D_Warrior/Assets/Scripts/Car.cs
+++ 2D_Warrior/Assets/Scripts/Car.cs
@@ -1,18 +1,22 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Car : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    // 單行註解：不會被程式讀取
+    // 欄位語法
+    // 修飾詞 類型 名稱；
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    // 四大類型
+    // 整數 int
+    // 浮點數 float
+    // 字串 string
+    // 布林值 bool
+
+    // 修飾詞
+    // 私人：不會顯示 (預設) private
+    // 公開：會顯示 public
+    public int height;
+    public float weight;
+    public string brand;
+    public bool hasWindow;
 }
