09efba6d765bce7799d8ffe0dbcdad785c720159
KID <50666207+KID421@users.noreply.github.com>
2020-12-20 14:40:54 +0800

    指定值

--- 2D_Warrior/Assets/Scripts/Car.cs
+++ 2D_Warrior/Assets/Scripts/Car.cs
@@ -4,7 +4,7 @@ public class Car : MonoBehaviour
 {
     // 單行註解：不會被程式讀取
     // 欄位語法
-    // 修飾詞 類型 名稱；
+    // 修飾詞 類型 名稱 (指定 值)；
 
     // 四大類型
     // 整數 int
@@ -15,8 +15,13 @@ public class Car : MonoBehaviour
     // 修飾詞
     // 私人：不會顯示 (預設) private
     // 公開：會顯示 public
-    public int height;
-    public float weight;
-    public string brand;
-    public bool hasWindow;
+
+    // 指定符號 =
+    public int height = 5;
+    public float weight = 5.5f;
+    public string brand = "BMW";
+
+    // 是 true
+    // 否 false
+    public bool hasWindow = true;       
 }
