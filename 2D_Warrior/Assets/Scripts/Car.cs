5456cfb1d76a5e978525c7e88c75519a49dae68f
KID <50666207+KID421@users.noreply.github.com>
2020-12-27 12:49:47 +0800

    方法參數

--- 2D_Warrior/Assets/Scripts/Car.cs
+++ 2D_Warrior/Assets/Scripts/Car.cs
@@ -123,12 +123,32 @@ public class Car : MonoBehaviour
         print("開車音效");
     }
 
-    private void Drive(string direction)
+    /// <summary>
+    /// 開車方法
+    /// </summary>
+    /// <param name="direction">方向</param>
+    /// <param name="sound">音效</param>
+    /// <param name="speed">速度</param>
+    private void Drive(string direction, string sound, int speed)
     {
         print("開車方向：" + direction);
-        print("開車音效");
+        print("開車音效：" + sound);
+        print("開車速度：" + speed);
     }
 
+    /// <summary>
+    /// 開啟雨刷
+    /// </summary>
+    /// <param name="speed">雨刷速度</param>
+    /// 參數預設值：選填式參數
+    /// 選填式參數必須寫在最右方
+    private void OpenBrush(string sound, int count = 2, int speed = 50)
+    {
+        print("開雨刷");
+        print("音效：" + sound);
+        print("雨刷的速度：" + speed);
+        print("雨刷的數量：" + count);
+    }
     #endregion
 
     // 名稱藍色：事件
@@ -162,8 +182,12 @@ public class Car : MonoBehaviour
         print(myName);
 
         // 呼叫時輸入的 引數
-        Drive("後面");
-        Drive("前面");
-        Drive("左邊");
+        Drive("後面", "噗噗噗", 100);
+        Drive("前面", "噗噗噗", 50);
+        Drive("左邊", "咻咻咻", 999);
+
+        OpenBrush("刷刷");
+        // 指定預設值參數 參數名稱: 值
+        OpenBrush("刷刷", speed: 500);
     }
 }
