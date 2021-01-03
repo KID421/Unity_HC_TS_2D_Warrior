5557b3720b02401cd86250546d9b024c25e2341d
KID <50666207+KID421@users.noreply.github.com>
2021-01-03 15:40:37 +0800

    添加物理元件

--- 2D_Warrior/Assets/Scripts/SceneControl.cs
+++ 2D_Warrior/Assets/Scripts/SceneControl.cs
@@ -1,8 +1,15 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;  // 引用 場景管理 API
 
+// C# : 繼承
+// 擁有此類別的成員 - 欄位、屬性、方法以及事件
 public class SceneControl : MonoBehaviour
 {
+    [Header("音效來源")]
+    public AudioSource aud;
+    [Header("按鈕音效")]
+    public AudioClip soundClick;
+
     // 1. 方法要讓按鈕呼叫必須設為公開 public
 
     /// <summary>
@@ -10,9 +17,10 @@ public class SceneControl : MonoBehaviour
     /// </summary>
     public void StartGame()
     {
-        // 2. 必須將場景放在 File > Build Settings...
-        // 場景管理器.載入場景("場景名稱")；
-        SceneManager.LoadScene("遊戲場景");
+        // 音效來源.播放一次(音效，音量)
+        aud.PlayOneShot(soundClick, 2);
+        // 延遲呼叫("方法名稱"，延遲秒數)
+        Invoke("DelayStartGame", 1.5f);
     }
 
     /// <summary>
@@ -20,13 +28,41 @@ public class SceneControl : MonoBehaviour
     /// </summary>
     public void BackToMenu()
     {
-        SceneManager.LoadScene("選單");
+        aud.PlayOneShot(soundClick, 2);
+        Invoke("DelayBackToMenu", 1.5f);
     }
 
     /// <summary>
     /// 離開遊戲
     /// </summary>
     public void QuitGame()
+    {
+        aud.PlayOneShot(soundClick, 2);
+        Invoke("DelayQuitGame", 1.5f);
+    }
+
+    /// <summary>
+    /// 延遲開始遊戲
+    /// </summary>
+    private void DelayStartGame()
+    {
+        // 2. 必須將場景放在 File > Build Settings...
+        // 場景管理器.載入場景("場景名稱")；
+        SceneManager.LoadScene("遊戲場景");
+    }
+
+    /// <summary>
+    /// 延遲返回選單
+    /// </summary>
+    private void DelayBackToMenu()
+    {
+        SceneManager.LoadScene("選單");
+    }
+
+    /// <summary>
+    /// 延遲離開遊戲
+    /// </summary>
+    private void DelayQuitGame()
     {
         // 應用程式.離開()
         Application.Quit();
