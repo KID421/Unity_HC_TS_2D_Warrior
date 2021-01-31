f8ca9c53cd68e7a00c2eb064c345767cfdd612bf
KID <50666207+KID421@users.noreply.github.com>
2021-01-31 16:18:36 +0800

    更新魔王觸發

---
+++ 2D_Warrior/Assets/Scripts/BossTrigger.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class BossTrigger : MonoBehaviour
+{
+    [Header("魔王血條")]
+    public GameObject objHp;
+    [Header("魔王")]
+    public GameObject objBoss;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.name == "戰士")
+        {
+            objHp.SetActive(true);
+            objBoss.SetActive(true);
+        }
+    }
+}
